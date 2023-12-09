1329b25bdb472eff2585891edf3dbaa99eb87c4a
isaiah <isaiahcodesforgood@gmail.com>
2023-12-09 10:31:40 -0800

    all get all song routes

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -6,16 +6,39 @@ import os
 song_routes = Blueprint('songs', __name__)
 
 
+#Get all songs
+
 @song_routes.route('/', methods=['GET'])
-def get_all_songs():
-    return '<h1>Homepage</h1>'
+def get_all_songs_no_order():
+    songs = Song.query.all()
+    all_songs = []
+    for song in songs:
+        all_songs.append(song.to_dict())
+    return all_songs
+
+@song_routes.route('/title', methods=['GET'])
+def get_all_songs_by_title():
+    ordered_songs = Song.query.order_by(Song.title)
+    all_songs = []
+    for song in ordered_songs:
+        all_songs.append(song.to_dict()) 
+    return all_songs   
+
+@song_routes.route('/uploadedat', methods=['GET'])
+def get_all_songs_by_upload_date():
+    ordered_songs = Song.query.order_by(Song.uploaded_at)
+    all_songs = []
+    for song in ordered_songs:
+        all_songs.append(song.to_dict()) 
+    return all_songs 
+    
     
 
 @song_routes.route('/:songId', methods=['GET'])
 def get_a_song(id):
     pass
 
-@song_routes.route('/play/<filename>', methods=[])
+@song_routes.route('/play/<filename>', methods=['GET'])
 def play_song():
     pass
 
