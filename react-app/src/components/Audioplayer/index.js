aee490460361f5b6137fc327a54122ffee6b650e
isaiah <isaiahcodesforgood@gmail.com>
2024-01-14 11:17:58 -0800

    onEnd, can play thru songs and some css

--- react-app/src/components/Audioplayer/index.js
+++ react-app/src/components/Audioplayer/index.js
@@ -3,27 +3,33 @@ import { IoMdPlay } from "react-icons/io";
 import { IoPlaySkipBackSharp } from "react-icons/io5";
 import { IoPlaySkipForwardSharp } from "react-icons/io5";
 import { IoPauseSharp } from "react-icons/io5";
+import { ImLoop } from "react-icons/im";
+import { BiSolidVolumeFull } from "react-icons/bi";
+import { ImVolumeMute2 } from "react-icons/im";
 import MyAudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import "./AudioPlayer.css";
 
 function AudioPlayer ({songsData, allSongs}) {  
-  // const [songs, setSongs] = useState(allSongs);
-  // const [isPlaying, setIsPlaying] = useState(false);
-  const [currentSong, setCurrentSong] = useState(allSongs[3].filepath);
- 
-  let songTitle;
-  let songArtist;
-  if (currentSong.includes(allSongs[3].title)) {
-    songTitle = allSongs[3].title
-    songArtist = allSongs[3].artist
-  }
+  let audioElem = useRef(null);
+  const [currentSongIdx, setCurrentSongIdx] = useState(2);
+  let songTitle = allSongs[currentSongIdx].title;
+  let songArtist = allSongs[currentSongIdx].artist;
 
+  const playNextSong = () => {
+    const nextIdx = (currentSongIdx + 1) % songsData.length;
+    setCurrentSongIdx(nextIdx);
+  }
+ 
   const customIcons = {
-    play: <IoMdPlay size={32} color="rgb(255,255,255)"/>,
-    pause: <IoPauseSharp size={32} color="rgb(255,255,255"/>,
-    previous: <IoPlaySkipBackSharp size={22} color="rgb(255,255,255"/>,
-    next: <IoPlaySkipForwardSharp size={22} color="rgb(255,255,255"/>
+    play: <IoMdPlay size={32} color="rgb(255,255,255)" />,
+    pause: <IoPauseSharp size={32} color="rgb(255,255,255" />,
+    previous: <IoPlaySkipBackSharp size={22} color="rgb(255,255,255" className="prev-btn"/>,
+    next: <IoPlaySkipForwardSharp size={22} color="rgb(255,255,255" className="next-btn"/>,
+    loop: <ImLoop size={22} color="rgb(255,255,255)"/>,
+    loopOff: <ImLoop size={22} color="#aaa"/>,
+    volume: <BiSolidVolumeFull />,
+    volumeMute: <ImVolumeMute2 />
   }
 
   const playerStyles = {
@@ -31,23 +37,22 @@ function AudioPlayer ({songsData, allSongs}) {
     width: '500px', 
   }
 
-  let audioElem = useRef(null);
-   
   return ( 
     <>
       <div id="audioplayer-cont">
         <div id="audio-content">
-          <img id="song-image" src={allSongs[3].imagepath} alt=""></img>
+          <img id="song-image" src={allSongs[currentSongIdx].imagepath} alt=""></img>
           <div id="song-info">
             <div id="song-title">{songTitle}</div>
             <div id="song-artist">{songArtist}</div>
           </div>
         </div>
         <div id="audio-player">
-          {songsData && songsData.length > 0 && currentSong && (
+          {songsData && songsData.length > 0 && songsData[currentSongIdx] && (
             <MyAudioPlayer 
-              src={currentSong}
+              src={songsData[currentSongIdx]}
               ref={audioElem}
+              onEnded={playNextSong}
               customIcons={customIcons}
               showSkipControls={true} 
               showJumpControls={false}
--- react-app/src/components/Sidebar/index.js
+++ react-app/src/components/Sidebar/index.js
@@ -5,6 +5,11 @@ function Sidebar () {
     <div id="sidebar-cont">
       <p>Sidebar</p>
       <h2>hellooooooooooooooooooooo</h2>
+      <p>Your Songs!</p>
+      <p>Albums</p>
+      <p>Playlists?</p>
+      <p>Likes?</p>
+      <p>Queue?</p>
     </div>
   );
 };
