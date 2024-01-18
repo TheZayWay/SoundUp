a0f39910a888b92e0e3c9c5e74e19c799c48a2be
isaiah <isaiahcodesforgood@gmail.com>
2024-01-17 18:16:41 -0800

    removed song seeders

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-from .songs import seed_songs, undo_songs
+# from .songs import seed_songs, undo_songs
 from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
@@ -10,14 +10,14 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        undo_songs()
+        # undo_songs()
         undo_users()
     seed_users()
-    seed_songs()
+    # seed_songs()
 
 
 @seed_commands.command('undo')
 def undo():
-    undo_songs()
+    # undo_songs()
     undo_users()
     
