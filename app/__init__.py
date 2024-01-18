58ff50a082b82ce48221711ae74a071eead7aaef
isaiah <isaiahcodesforgood@gmail.com>
2024-01-17 17:18:48 -0800

    .env in migrations update

--- app/__init__.py
+++ app/__init__.py
@@ -219,7 +219,7 @@ def react_root(path):
   """
   if path == 'favicon.ico':
       return app.send_from_directory('public', 'favicon.ico')
-  # return app.send_static_file('index.html')
+  return app.send_static_file('index.html')
 
 # 404
 @app.errorhandler(404)
