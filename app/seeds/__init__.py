2fe78e9cf83c41a2fe407877f99e85acfd46b944
isaiah <isaiahcodesforgood@gmail.com>
2024-01-17 18:34:57 -0800

    new migration

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,5 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-# from .songs import seed_songs, undo_songs
 from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
@@ -10,14 +9,12 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # undo_songs()
         undo_users()
     seed_users()
-    # seed_songs()
+
 
 
 @seed_commands.command('undo')
 def undo():
-    # undo_songs()
     undo_users()
     
