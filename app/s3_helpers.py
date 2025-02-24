afdf04c87c7995ffa5cfd9f0a018722264b18433
isaiah <isaiahcodesforgood@gmail.com>
2025-02-23 17:50:29 -0800

    reverted

--- app/s3_helpers.py
+++ app/s3_helpers.py
@@ -2,10 +2,8 @@ import boto3
 import botocore
 import os
 
-# BUCKET_NAME = os.environ.get("S3_BUCKET")
-BUCKET_NAME = 'soundup'
-# S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
-S3_LOCATION = f"https://soundup.s3.amazonaws.com/"
+BUCKET_NAME = os.environ.get("S3_BUCKET")
+S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
 
 s3 = boto3.client(
     "s3",
